50152ed8c599382f29d5921ca4aad62347277252
Clay Rardin <clayton.rardin@gmail.com>
2017-10-28 13:28:18 -0600

    Initial upload

---
+++ empty_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+class FooTests(unittest.TestCase):
+
+    def testFoo(self):
+        self.failUnless(True)
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
+
